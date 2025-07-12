00405eae2658d38a09a783caf33d4d84f813504c
csvkbcq <prajwaln395@gmail.com>
2025-07-12 18:17:25 +0530

    thirdCommitSquash

--- sub.java
+++ sub.java
@@ -1,8 +1,8 @@
 public class sub {
     public static void main(String[] args) {
-        int a = 10;
+        int x = 10;
         int b = 5;
-        int res = a - b;
+        int res = x - b;
         System.out.println("Subqrrw123tebgttt: " + res);
     }
 }
