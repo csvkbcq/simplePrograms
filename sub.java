977db0dc800cc9fabca36e9c417a6f4bdf42f204
csvkbcq <prajwaln395@gmail.com>
2025-07-12 17:44:06 +0530

    textChanged

--- sub.java
+++ sub.java
@@ -3,6 +3,6 @@ public class sub {
         int a = 10;
         int b = 5;
         int result = a - b;
-        System.out.println("Subtraction: " + result);
+        System.out.println("Sub: " + result);
     }
 }
