b64d73252bae5e62d58d7dcc5672655605e12196
csvkbcq <prajwaln395@gmail.com>
2025-07-12 18:19:05 +0530

    secondCommit

--- sub.java
+++ sub.java
@@ -1,8 +1,8 @@
 public class sub {
     public static void main(String[] args) {
         int x = 10;
-        int b = 5;
-        int res = x - b;
+        int y = 5;
+        int res = x - y;
         System.out.println("Subqrrw123tebgttt: " + res);
     }
 }
